0ed7ce18893fc216e264c2c4b38db3cc05bbabbb
small T <zhaodi@zhaodidemacbook-air-2.local>
2017-03-12 14:07:04 +0800

    初始化项目

---
+++ duoxiancheng/duoxiancheng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  duoxiancheng
+//
+//  Created by small T on 17/3/12.
+//  Copyright © 2017年 guojianlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ duoxiancheng/duoxiancheng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  duoxiancheng
+//
+//  Created by small T on 17/3/12.
+//  Copyright © 2017年 guojianlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
